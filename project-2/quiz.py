1bc71fd2772f5dd1895ab8a86d896721064a9014
이현민 <dlgusals01205864@c5r9s7.codyssey.kr>
2026-04-07 18:02:50 +0900

    메서드 오류 해결

--- project-2/quiz.py
+++ project-2/quiz.py
@@ -4,23 +4,23 @@ class Quiz:
         self.choices = choices
         self.answer = answer
 
-        def display(self):
-            print(f"/n문제: {self.question}" )
+    def display(self):
+            print(f"\n문제: {self.question}" )
             for idx, choice in enumerate(self.choices, start=1):
                 print(f"{idx}. {choice}")
 
-        def is_correct(self, user_answer):
+    def is_correct(self, user_answer):
             return user_answer == self.answer
         
-        def to_dict(self):
+    def to_dict(self):
             return {
                 "question": self.question,
                 "choices": self.choices,
                 "answer": self.answer,
             }
         
-        @classmethod
-        def from_dict(cls, data):
+    @classmethod
+    def from_dict(cls, data):
             return cls(
                 question = data["question"],
                 choices = data["choices"],
