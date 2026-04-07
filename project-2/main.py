098570a4d6e2c4672ff9ad60ed76af2bb2651c2c
이현민 <dlgusals01205864@c5r9s7.codyssey.kr>
2026-04-07 17:40:44 +0900

    main.py 생성, 예외 사항추가

---
+++ project-2/main.py
@@ -0,0 +1,11 @@
+from quiz_game import QuizGame
+
+def main():
+    game = QuizGame
+    game.run()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (KeyboardInterrupt, EOFError):
+        print("\n프로그램이 안전하게 종료되었습니다.")
